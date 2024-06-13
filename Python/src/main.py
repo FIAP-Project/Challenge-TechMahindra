9322e6198c249b499d303f97aea8b4d0fc6232a4
Felipe <felipecerboncinix@gmail.com>
2024-06-13 09:57:34 -0300

    Randomize all quiz question, get user answer and count correct answers

--- Python/src/main.py
+++ Python/src/main.py
@@ -9,6 +9,17 @@ def main():
     topic: str = topic_handler.get_topic(language)
 
     quiz: dict = quiz_handler.load_quiz(topic, language)
+    quiz = quiz_handler.randomize_quiz(quiz['quiz'])
+
+    correct_count = 0
+    for i, q in enumerate(quiz):
+        quiz_handler.print_question(q, i)
+        user_answer = quiz_handler.get_user_answer(i)
+
+        if quiz_handler.check_answer(q, user_answer):
+            correct_count += 1
+
+    print(language_handler.get_translatable('quiz.correct.answers.amount', language).format(correct_count, len(quiz)))
 
 
 if __name__ == "__main__":
--- Python/src/quiz/quiz_handler.py
+++ Python/src/quiz/quiz_handler.py
@@ -1,12 +1,20 @@
 import json
+import random
 
 from Python.src.quiz.language_handler import get_quiz_translation
+from Python.src.quiz.language_handler import get_translatable
+from Python.src.util import print_utils
 from Python.src.util.constants import ROOT_DIR
+
+_language = ''
 
 
 def load_quiz(topic: str, language: str) -> dict:
     quiz: dict = _get_quiz_by_topic(topic)
     translation: dict = get_quiz_translation(language)
+
+    global _language
+    _language = language
 
     return _translate_quiz(quiz, translation)
 
@@ -28,3 +36,36 @@ def _translate_quiz(quiz: dict, translation: dict):
 
     else:
         return quiz
+
+
+def randomize_quiz(quiz) -> dict:
+    random.shuffle(quiz)  # Randomize the order of questions
+    for q in quiz:
+        random.shuffle(q['options'])  # Randomize the order of options
+    return quiz
+
+
+def print_question(q, q_index):
+    print(f"Q{q_index + 1}: {q['question']}")
+    for i, option in enumerate(q['options']):
+        print(f"  {i + 1}. {option}")
+    print()
+
+
+def get_user_answer(q_index):
+    while True:
+        try:
+            answer = int(input(get_translatable('quiz.question.answer', _language).format(f'Q{q_index + 1}')).strip())
+            print()
+            if 1 <= answer <= 4:
+                return answer
+            else:
+                print_utils.print_error(f'{get_translatable("quiz.wrong.number.selection", _language)} 1~4')
+        except ValueError:
+            print_utils.print_error(f'{get_translatable("quiz.wrong.number.selection", _language)} 1~4')
+
+
+def check_answer(q, user_answer):
+    correct_answer = q['correct_answer']
+    correct_option_index = q['options'].index(correct_answer) + 1
+    return user_answer == correct_option_index
